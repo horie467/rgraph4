82755cd2764859f18b97e6b16f019764041143ad
horie467 <horie.nobuyuki@gmail.com>
2019-09-21 11:03:17 +0900

    test

--- graphpanel4.rb
+++ graphpanel4.rb
@@ -1,7 +1,7 @@
 =begin
 ** Form generated from reading ui file 'graphpanel4.ui'
 **
-** Created: 水 9月 11 17:14:10 2019
+** Created: 土 9月 21 11:00:53 2019
 **      by: Qt User Interface Compiler version 4.8.7
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
--- graphwindow4.rb
+++ graphwindow4.rb
@@ -1,7 +1,7 @@
 =begin
 ** Form generated from reading ui file 'graphwindow4.ui'
 **
-** Created: 水 9月 11 17:14:11 2019
+** Created: 土 9月 21 11:00:55 2019
 **      by: Qt User Interface Compiler version 4.8.7
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
